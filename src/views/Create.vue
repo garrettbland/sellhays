fbea08277840d72251e57c0f04bc7c559a249311
garrettbland <gmorganbland@gmail.com>
2019-03-15 01:50:36 -0500

    updated create styling

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -23,13 +23,13 @@
 		    <!-- row -->
 		    <div class="flex justify-between items-center">
 		    	<div class="w-3/5 pr-4">
-		    		<div>
+		    		<div class="text-grey-dark uppercase text-sm px-2 py-2">
 		    			Address
 		    		</div>
 		      		<input v-model="sale.address" placeholder="Street Address" class="w-full bg-grey-light p-3 text-2xl text-black rounded-lg focus:outline-none focus:bg-white border-2 border-transparent hover:border-grey focus:border-grey"/>
 		      	</div>
 		      	<div class="w-2/5">
-		      		<div>
+		      		<div class="text-grey-dark uppercase text-sm px-2 py-2">
 		    			City
 		    		</div>
 		      		<div class="flex bg-grey-lighter p-3 text-2xl text-grey-darker opacity-75 rounded-lg focus:outline-none border-2 border-transparent">
@@ -41,19 +41,19 @@
 		    <!-- row -->
 		    <div class="flex justify-between items-center mt-8">
 		    	<div class="w-1/2 pr-4">
-		    		<div>
+		    		<div class="text-grey-dark uppercase text-sm px-2 py-2">
 		    			Date of Sale
 		    		</div>
 		      		<input v-model="sale.date.start" placeholder="Aug 5, 2019" class="w-full bg-grey-light p-3 text-2xl text-black rounded-lg focus:outline-none focus:bg-white border-2 border-transparent hover:border-grey focus:border-grey"/>
 		      	</div>
 		      	<div class="w-1/4 pr-4">
-		      		<div>
+		      		<div class="text-grey-dark uppercase text-sm px-2 py-2">
 		    			Start Time
 		    		</div>
 		      		<input v-model="sale.time.start" placeholder="2:00 PM" class="w-full bg-grey-light p-3 text-2xl text-black rounded-lg focus:outline-none focus:bg-white border-2 border-transparent hover:border-grey focus:border-grey"/>
 		      	</div>
 		      	<div class="w-1/4">
-		      		<div>
+		      		<div class="text-grey-dark uppercase text-sm px-2 py-2">
 		    			End Time
 		    		</div>
 		      		<input v-model="sale.time.end" placeholder="7:00 PM" class="w-full bg-grey-light p-3 text-2xl text-black rounded-lg focus:outline-none focus:bg-white border-2 border-transparent hover:border-grey focus:border-grey"/>
@@ -63,49 +63,54 @@
 		    <!-- row -->
 		    <div class="flex justify-between items-center mt-8">
 		      	<div class="w-full">
-		      		<VueTrix v-model="sale.description" placeholder="Description of your sale" @trix-file-accept.prevent/>
+		      		<VueTrix v-model="sale.description" placeholder="Description of your sale, popular items, contact info, ect" @trix-file-accept.prevent/>
 		      	</div>
 		    </div>
 
 		   	<!-- row -->
-		    <div class="flex flex-wrap justify-between items-center mt-8">
-		    	<div class="w-full">
+		    <div class="flex flex-col mt-8">
+		    	<div class="w-full text-grey-dark uppercase text-sm px-2 py-2">
 	    			Images (max of 3)
 	    		</div>
-		      	<div class="w-1/3 px-2" v-for="(image,index) in tempImages">
-		      		<div class="flex justify-center bg-grey-lightest border-2 border-dashed border-grey rounded-lg hover:border-purple cursor-pointer">
-		      			<div>
-		      				<img :src="getImage(image)" />
-							<button @click="removeImage(index)">Remove image</button>
-		      			</div>
-		      		</div>
-		      	</div>
-		      	<div class="w-1/3 px-2">
-		      		<div class="flex justify-center bg-grey-lightest border-2 border-dashed border-grey rounded-lg hover:border-purple cursor-pointer">
+	    		<div class="flex flex-wrap">
+			      <div class="w-1/3 px-2" v-for="(image,index) in tempImages">
+			      	<div class="flex justify-center bg-grey-lightest rounded-lg relative">
+			      		<div>
+			      			<img :src="getImage(image)" class="z-10 shadow-md rounded-lg"/>
+							<button @click="removeImage(index)" class="absolute pin-t pin-l z-20 hover:opacity-50 focus:outline-none outline-none cursor-pointer">
+								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-8 icon-close-circle"><circle cx="12" cy="12" r="10" class="trash-primary"></circle><path class="secondary" d="M13.41 12l2.83 2.83a1 1 0 0 1-1.41 1.41L12 13.41l-2.83 2.83a1 1 0 1 1-1.41-1.41L10.59 12 7.76 9.17a1 1 0 0 1 1.41-1.41L12 10.59l2.83-2.83a1 1 0 0 1 1.41 1.41L13.41 12z"></path></svg>
+							</button>
+			      		</div>
+			      	</div>
+			      </div>
+			      <div class="w-1/3 px-2" v-if="tempImages.length < 3">
+		      		<div class="flex justify-center bg-grey-lightest border-2 border-dashed border-grey rounded-lg hover:border-purple cursor-pointer" @click="chooseImage()">
 		      			<div class="py-12 text-grey-darker opacity-75">
 		      				Click here to add image
-		      				<input type="file" @change="onFileChange">
+		      				<input type="file" ref="addImage" hidden @change="onFileChange">
 		      			</div>
 		      		</div>
-		      	</div>
+		      	  </div>
+			    </div>
 		    </div>
 
 		    <!-- row -->
-		    <div class="flex justify-between items-center mt-8">
+<!-- 		    <div class="flex justify-between items-center mt-8">
 		      	<div class="w-full">
 		      		<input v-model="tag" placeholder="tag"><button @click="addTag()">Add +</button>
 					<ul>
 						<li v-for="(tag,index) in sale.tags">{{tag}} <button @click="removeTag(index)">X</button></li>
 					</ul>
 		      	</div>
-		    </div>
+		    </div> -->
     
 		    </div>
 		  </div>
 
-		<div class="max-w-xl mx-auto flex justify-end">
-			<button @click="createSale()" class="rounded-lg shadow-lg p-4 bg-purple">
-				Create
+		<div class="max-w-xl mx-auto flex justify-end font-sans mb-12">
+			<button @click="createSale()" class="flex items-center p-2 bg-purple border-2 border-transparent hover:bg-purple-dark rounded-lg no-underline focus:outline-none shadow-lg mt-4">
+				<span class="px-3 text-white text-xl">Submit</span>
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-8 icon-check"><circle cx="12" cy="12" r="10" class="primary"></circle><path class="secondary" d="M10 14.59l6.3-6.3a1 1 0 0 1 1.4 1.42l-7 7a1 1 0 0 1-1.4 0l-3-3a1 1 0 0 1 1.4-1.42l2.3 2.3z"></path></svg>
 			</button>
 		</div>
 
@@ -152,6 +157,11 @@ export default {
 		}
 	},
 	methods:{
+		chooseImage(){
+
+			this.$refs.addImage.click()
+
+		},
 		goToSale(){
 
 			// Route user to the newly created sale
